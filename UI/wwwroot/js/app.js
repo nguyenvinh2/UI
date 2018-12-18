dd67e81a0d9a4844b28db03a7b0ffde8b07f438e
nguye <nguyenv2@outlook.com>
2018-12-18 12:05:20 -0800

    first part complete

---
+++ UI/wwwroot/js/app.js
@@ -0,0 +1,13 @@
+﻿function openNav() {
+  $('#side-nav').width('200px');
+  $(document.body).css('background-color', 'rgba(0,0,0,0.5)');
+}
+
+function closeNav() {
+  $('#side-nav').width('0px');
+  $(document.body).css('background-color', 'white');
+}
+
+
+
+
